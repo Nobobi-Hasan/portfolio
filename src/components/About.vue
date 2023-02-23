9affe35f68f112bf9f6196b4bb180d6163a2d71d
Nobobi-Hasan <shatin.hasan73@gmail.com>
2023-02-23 15:25:52 +0600

    about me

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,11 @@
 <template>
 
-    <section id="about" class="grid place-items-center pt-32 mt-20 overflow-hidden bg-black">
+    <section id="about" class="grid place-items-center pt-28 mt-20 overflow-hidden bg-black">
         <div class="container overflow-x-hidden">
 
             <div class=" grid grid-flow-row-dense grid-cols-1 lg:grid-cols-2 items-start gap-20 lg:gap-16 w-full px-0 lg:px-10">
 
-                <div class="font-bold text-black w-full pb-10 order-1">
+                <div class="font-bold text-black w-full py-10 order-1">
 
                     <p class="text-white text-md text-4xl">Hello, I am</p>
                     <h1 class="text-6xl text-highlight mt-10 mb-16 tracking-wider">&lt; Md. Nabobi <br> Hasan /&gt;</h1>
@@ -16,15 +16,42 @@
                 <div class=" w-full h-full order-2 flex items-center justify-center relative">
 
 
-                    <img src="../assets/Ellipse 4.png" alt="" class="w-[70%] ">
-                    <img src="../assets/avatar.png" alt="" class="w-[50%] absolute pb-14 pr-3">
+                    <img src="../assets/Ellipse 4.png" alt="" class="w-[80%] ">
+                    <img src="../assets/avatar.png" alt="" class="w-[60%] absolute pb-14 pr-3">
 
                 </div>
 
             </div>
-        </div>
+            
 
-        
+            <!-- About me part -->
+
+            <div class="grid grid-flow-row-dense grid-cols-2 lg:grid-cols-3 items-start gap-20 lg:gap-16 w-full px-0 lg:px-10 py-24">
+            <!-- <div class="items-start gap-20 lg:gap-16 w-full px-0 lg:px-10 py-16 flex flex-col lg:flex-row"> -->
+
+                <div class="font-bold col-span-2 w-full py-10 order-1">
+
+                    <h1 class="text-5xl text-highlight mb-8 tracking-wider">aboutMe<span class="text-white">( )</span></h1>
+
+                    <p class="text-white text-xl tracking-wide font-medium text-justify">I am Md. Nabobi Hasan, a recent graduate from American International University-Bangladesh with a degree in Computer Science and Engineering. Completed 3 months of internship experience as a Software Engineer Intern at ENKAIZEN. As a Full Stack Web Developer, I am specialized in Laravel as a backend and Vue.js as a frontend framework, along with experience in CSS frameworks like Bootstrap and Tailwind. I am motivated self-starter with excellent communication skills and a team player mindset, eager to learn new technologies and frameworks to contribute to organizational growth.</p>
+
+                </div>
+
+                <div class="font-bold text-white col-span-2 lg:col-span-1 w-full py-10 order-2 flex flex-col gap-5">
+
+                    <div class="bg-zinc-700 w-full h-32 rounded-md flex justify-center items-center">
+                        <h1 class="text-3xl lg:text-2xl tracking-wider text-center">Fullstack Developer<span class="text-highlight tracking-wider">&lt;/&gt;</span></h1>
+                    </div>
+
+                    <div class="bg-zinc-700 w-full h-32 rounded-md flex justify-center items-center">
+                        <h1 class="text-3xl lg:text-2xl tracking-wider text-center">Fullstack Developer<span class="text-highlight tracking-wider">&lt;/&gt;</span></h1>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
 
     </section>
 
